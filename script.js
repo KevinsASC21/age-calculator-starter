ff672e452cfe916a64cbc0c8847075f779e05a63
Glitch (meadow-jolly-breakfast) <none>
2021-11-10 23:27:15 +0000

    🍙🎩 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let userName = "";
+let userBirthYear = 0;
+let userCurrentYear = 0;
+let age = 0;
+    
+$(".calcAge").click(function() {
+  
+  userBirthYear = $(".year").val();
+  userName = $(".name").val();
+  userCurrentYear = $(".currentYear").val();
+  
+  console.log(userBirthYear);
+  console.log(userName);
+  console.log(userCurrentYear);
+  
+  age = userCurrentYear - userBirthYear;
+  console.log(age);
 
+  $(".message").text(userName + " it looks like you are " + age + " year(s) old.");
+  
+});
